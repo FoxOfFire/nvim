32f9768509b069fee5961e7a59cb20c7c610c446
FoxOfFire <54199935+FoxOfFire@users.noreply.github.com>
2025-09-13 08:56:07 +0200

    java-lsp

--- init.lua
+++ init.lua
@@ -639,6 +639,17 @@ require('lazy').setup({
           },
         },
         rust_analyzer = {},
+        nextflow_ls = {
+          cmd = { 'java', '-jar', 'nextflow-language-server-all.jar' },
+            filetypes = { 'nextflow' },
+              settings = {
+                nextflow = {
+                  files = {
+                    exclude = { '.git', '.nf-test', 'work' },
+                  },
+                },
+              },
+          },
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
