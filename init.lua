91e5eba6787ae29ffbb1ad741793f94e69082a17
FoxOfFire <54199935+FoxOfFire@users.noreply.github.com>
2025-09-18 09:35:36 +0200

    pylint

--- init.lua
+++ init.lua
@@ -602,6 +602,8 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         clangd = {},
+        mypy = {},
+        flake8 = {},
         gopls = {
           cmd = { 'gopls' },
           filetypes = { 'go', 'gomod', 'gowork', 'gotmpl' },
